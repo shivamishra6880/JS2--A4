d86e007668ad183fea96a02fda54272986d4c139
shivamishra6880 <147376259+shivamishra6880@users.noreply.github.com>
2024-05-25 13:11:05 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,52 @@
+//  4.
+let a = ("Prep");
+let b = ("bytes")
+let c = a.concat(b)
+console.log(c)
+
+//  5.
+let shiva = ("shiva");
+console.log(shiva.length)
+
+// 6.
+// let adi
+
+// 7.
+let d = ("shiva");
+let e = ("mishra")
+let f = d + e;
+console.log(f)
+
+// 8.
+
+// let g ="naman"
+// //  prompt("enter name")
+// console.log(isPalindrome(g));
+
+// 9.
+let h = ("hello dosto kaise ho aap log");
+let i = h.split("");
+let j = i.reverse();
+let k= j.join("");
+console.log(k);
+
+// 10.
+
+let l = ("shiva")
+let m = ("shiva")
+if(l===m){
+   console.log("yes")
+}
+else{
+   console.log("no")
+}
+
+// 11.
+let n = ("Hi, I am You.You Prepbytes")
+let o = n.replaceAll("You","")
+console.log(o);
+
+// 12.
+let p = ("Hi, I am You. You Prepbytes")
+let q = p.split("");
+console.log(q)
